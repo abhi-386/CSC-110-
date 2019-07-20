c2f53120bdf85e24cb80f738be38cec6f1bef9c8
ABHISHEK AGARWAL <abhishekagarwal488@gmail.com>
2019-07-20 14:56:58 -0700

    Add files via upload
    Print the sentence with the help of '\n' to print them on different lines.

---
+++ ist prgm.py
@@ -0,0 +1,4 @@
+'''
+    Print the sentaenc ewith th ehelp of '\n' to print them on different lines.
+'''
+print('''There's a difference in living and living well.\nYou can't have it all by yourself.\nSomething's always missing till you share it with someone else.\nThere's a difference in living and living well. ''' )
